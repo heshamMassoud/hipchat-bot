a1bf79850c6121541055d39ec17580b77604a3c7
Hesham Massoud <heshamhamdymassoud@gmail.com>
2018-06-09 00:20:39 +0300

    Add new action targets.

---
+++ src/main/java/com/heshammassoud/controller/CommercetoolsMenuChoiceController.java
@@ -0,0 +1,31 @@
+package com.heshammassoud.controller;
+
+import com.atlassian.stride.model.webhooks.MessageSent;
+import com.atlassian.stride.spring.auth.AuthorizeJwtHeader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.annotation.Nonnull;
+
+@Controller
+public class CommercetoolsMenuChoiceController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DescriptorController.class);
+
+    public CommercetoolsMenuChoiceController() {
+    }
+
+    @AuthorizeJwtHeader
+    @PostMapping(path = "/commercetools-menu", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void mention(@RequestBody @Nonnull final MessageSent messageSent) {
+        LOGGER.info("Chose the commercetools menu controller", messageSent.getMessage().getText());
+    }
+
+}
