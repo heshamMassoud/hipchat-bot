f98f6fa1d42925a487a8f0892af8569b940a2dfe
Hesham Massoud <heshamhamdymassoud@gmail.com>
2018-06-09 02:31:27 +0300

    Fix action target controllers.

--- src/main/java/com/heshammassoud/controller/TableTennisMenuChoiceController.java
+++ src/main/java/com/heshammassoud/controller/TableTennisMenuChoiceController.java
@@ -1,31 +1,26 @@
 package com.heshammassoud.controller;
 
-import com.atlassian.stride.model.webhooks.MessageSent;
 import com.atlassian.stride.spring.auth.AuthorizeJwtHeader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-import javax.annotation.Nonnull;
 
 @Controller
 public class TableTennisMenuChoiceController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(DescriptorController.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(TableTennisMenuChoiceController.class);
 
     public TableTennisMenuChoiceController() {
     }
 
     @AuthorizeJwtHeader
-    @PostMapping(path = "/table-tennis-menu", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path = "/table-tennis-menu")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void mention(@RequestBody @Nonnull final MessageSent messageSent) {
-        LOGGER.info("Chose the table tennis menu controller", messageSent.getMessage().getText());
+    public void choose() {
+        LOGGER.info("Chose the table tennis menu controller");
     }
 
 }
