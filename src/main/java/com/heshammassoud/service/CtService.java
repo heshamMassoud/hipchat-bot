78f99a1941eb2f319ac4063fb63168f6276e22dd
Hesham Massoud <heshamhamdymassoud@gmail.com>
2018-06-08 17:48:14 +0300

    Test a rich formatted message.

--- src/main/java/com/heshammassoud/service/CtService.java
+++ src/main/java/com/heshammassoud/service/CtService.java
@@ -1,6 +1,7 @@
 package com.heshammassoud.service;
 
 import com.atlassian.adf.Document;
+import com.atlassian.adf.block.codeblock.Language;
 import com.atlassian.stride.api.StrideClient;
 import com.atlassian.stride.model.context.UserContext;
 import com.atlassian.stride.model.webhooks.MessageSent;
@@ -24,6 +25,7 @@ public class CtService {
 
     /**
      * TODO.
+     *
      * @param messageSent TODO.
      */
     public void reply(@Nonnull final MessageSent messageSent) {
@@ -39,7 +41,7 @@ public class CtService {
                     .get()
                     .from(userContext)
                     .thenCompose(userDetail -> strideClient.message()
-                                                           .send(buildMainMenuMessage(userDetail.getDisplayName()))
+                                                           .send(buildRichFormattedMessage())
                                                            .toUser(userContext))
                     .thenCompose(userDetail -> strideClient.message()
                                                            .send(document2)
@@ -56,6 +58,46 @@ public class CtService {
                        .orderedList(l -> l
                                .item(i -> i.paragraph("Play around with commercetools project data."))
                                .item(i -> i.paragraph("Play table tennis.")));
+    }
+
+    private Document buildRichFormattedMessage() {
+        return Document.create().paragraph(p -> p
+                .text("Here is some ")
+                .strong("bold test")
+                .text(" and ")
+                .em("text in italics")
+                .text(" as well as ")
+                .link(" a link", "https://www.atlassian.com")
+                .text(" , emojis ")
+                .emoji(":smile:")
+                .emoji(":rofl:")
+                .emoji(":nerd:")
+                .text(" and some code: ")
+                .code("var i = 0;")
+                .text(" and a bullet list"))
+                       .bulletList(b -> b
+                               .item("With one bullet point")
+                               .item("And another"))
+                       .info(p -> p
+                               .paragraph("and an info panel with some text, with some more code below"))
+                       .code(Language.javascript, "var i = 0;\nwhile(true) {\n  i++;\n}")
+                       .paragraph("And a card")
+                       .card("With a title", c -> c.attrs()
+                                                   .link("https://www.atlassian.com")
+                                                   .description("With some description, and a couple of attributes")
+                                                   .background("https://www.atlassian.com")
+                                                   .detail(d -> d
+                                                           .title("Type")
+                                                           .text("Task")
+                                                           .icon("https://ecosystem.atlassian.net/secure/"
+                                                                   + "viewavatar?size=xsmall&avatarId"
+                                                                   + "=15318&avatarType=issuetype", "Task"))
+                                                   .detail(d -> d
+                                                           .title("User")
+                                                           .text("Joe Blog")
+                                                           .icon("https://ecosystem.atlassian.net/secure/"
+                                                                   + "viewavatar?size=xsmall&avatarId="
+                                                                   + "15318&avatarType=issuetype", "Task")));
     }
 
 }
