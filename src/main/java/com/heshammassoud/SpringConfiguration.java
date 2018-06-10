83b15b4b431df9918ff263860cbee7e32a30786f
Hesham Massoud <heshamhamdymassoud@gmail.com>
2018-06-11 02:03:29 +0300

    Inject SphereClient on startup.

--- src/main/java/com/heshammassoud/SpringConfiguration.java
+++ src/main/java/com/heshammassoud/SpringConfiguration.java
@@ -1,11 +1,17 @@
 package com.heshammassoud;
 
 import com.atlassian.stride.spring.StrideSpringConfiguration;
+import com.heshammassoud.util.commercetools.SphereClientUtils;
+import io.sphere.sdk.client.SphereClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import(StrideSpringConfiguration.class)
 public class SpringConfiguration {
-    
+    @Bean
+    public SphereClient sphereClient(){
+        return SphereClientUtils.CTP_CLIENT;
+    }
 }
