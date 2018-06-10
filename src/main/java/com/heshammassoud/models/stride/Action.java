d1f8ff1a10928890c9d40604a9360fe49ef945db
Hesham Massoud <heshamhamdymassoud@gmail.com>
2018-06-10 22:06:06 +0300

    Revert "Remove parameters."
    This reverts commit f4166f5ee6409d641488ff945fc59ae8ecd7ba01.

--- src/main/java/com/heshammassoud/models/stride/Action.java
+++ src/main/java/com/heshammassoud/models/stride/Action.java
@@ -1,12 +1,16 @@
 package com.heshammassoud.models.stride;
 
 import javax.annotation.Nonnull;
+import java.util.Map;
 
 public class Action {
     private ActionTarget target;
+    private Map<String, String> parameters;
 
-    public Action(@Nonnull final String targetKey) {
+    public Action(@Nonnull final String targetKey,
+                  @Nonnull final Map<String, String> parameters) {
         this.target = new ActionTarget(targetKey);
+        this.parameters = parameters;
     }
 
     public ActionTarget getTarget() {
@@ -15,5 +19,13 @@ public class Action {
 
     public void setTarget(@Nonnull final ActionTarget target) {
         this.target = target;
+    }
+
+    public Map<String, String> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(@Nonnull final Map<String, String> parameters) {
+        this.parameters = parameters;
     }
 }
--- src/main/java/com/heshammassoud/models/stride/ActionGroupAction.java
+++ src/main/java/com/heshammassoud/models/stride/ActionGroupAction.java
@@ -36,7 +36,7 @@ public class ActionGroupAction {
             @Nonnull final String targetKey,
             @Nonnull final Map<String, String> parameters) {
 
-        final Action action = new Action(targetKey);
+        final Action action = new Action(targetKey, parameters);
         return new ActionGroupAction(key, title, appearance, action);
     }
 
